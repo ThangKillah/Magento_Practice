58159ce9aaedd85fa5384ada5a705afebeb97bd7
Bui Toan Thang <vietnga1910@gmail.com>
2018-07-06 11:23:39 +0700

    Them sua xoa post trong backend

---
+++ Magento213/app/code/OpenTechiz/Blog/Block/Adminhtml/Post/Edit/Form.php
@@ -0,0 +1,111 @@
+<?php
+namespace OpenTechiz\Blog\Block\Adminhtml\Post\Edit;
+
+
+class Form extends \Magento\Backend\Block\Widget\Form\Generic
+{
+
+    protected $_systemStore;
+
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Data\FormFactory $formFactory,
+        \Magento\Store\Model\System\Store $systemStore,
+        array $data = []
+    ) {
+        $this->_systemStore = $systemStore;
+        parent::__construct($context, $registry, $formFactory, $data);
+    }
+
+    /**
+     * Init form
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setId('post_form');
+        $this->setTitle(__('Post Information'));
+    }
+
+    /**
+     * Prepare form
+     *
+     * @return $this
+     */
+    protected function _prepareForm()
+    {
+        /** @var \OpenTechiz\Blog\Model\Post $model */
+        $model = $this->_coreRegistry->registry('blog_post');
+
+        /** @var \Magento\Framework\Data\Form $form */
+        $form = $this->_formFactory->create(
+            ['data' => ['id' => 'edit_form', 'action' => $this->getData('action'), 'method' => 'post']]
+        );
+
+        $form->setHtmlIdPrefix('post_');
+
+        $fieldset = $form->addFieldset(
+            'base_fieldset',
+            ['legend' => __('General Information'), 'class' => 'fieldset-wide']
+        );
+
+        if ($model->getPostId()) {
+            $fieldset->addField('post_id', 'hidden', ['name' => 'post_id']);
+        }
+
+        $fieldset->addField(
+            'title',
+            'text',
+            ['name' => 'title', 'label' => __('Post Title'), 'title' => __('Post Title'), 'required' => true]
+        );
+
+        $fieldset->addField(
+            'url_key',
+            'text',
+            [
+                'name' => 'url_key',
+                'label' => __('URL Key'),
+                'title' => __('URL Key'),
+                'required' => true,
+                'class' => 'validate-xml-identifier'
+            ]
+        );
+
+        $fieldset->addField(
+            'status',
+            'select',
+            [
+                'label' => __('Status'),
+                'title' => __('Status'),
+                'name' => 'status',
+                'required' => true,
+                'options' => ['1' => __('Enabled'), '0' => __('Disabled')]
+            ]
+        );
+        if (!$model->getId()) {
+            $model->setData('status', '1');
+        }
+
+        $fieldset->addField(
+            'content',
+            'editor',
+            [
+                'name' => 'content',
+                'label' => __('Content'),
+                'title' => __('Content'),
+                'style' => 'height:36em',
+                'required' => true
+            ]
+        );
+
+        $form->setValues($model->getData());
+        $form->setUseContainer(true);
+        $this->setForm($form);
+
+        return parent::_prepareForm();
+    }
+}
