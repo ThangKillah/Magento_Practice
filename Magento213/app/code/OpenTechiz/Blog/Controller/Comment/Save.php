e91277aabe2c6672296b56c59b84269fa0f24eab
Bui Toan Thang <vietnga1910@gmail.com>
2018-07-08 20:45:29 +0700

    Chỉnh sửa DC thêm các field email, pending, deny + add comment dùng ajax

--- Magento213/app/code/OpenTechiz/Blog/Controller/Comment/Save.php
+++ Magento213/app/code/OpenTechiz/Blog/Controller/Comment/Save.php
@@ -3,46 +3,82 @@
 namespace OpenTechiz\Blog\Controller\Comment;
 
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\Controller\ResultFactory;
-
+use Magento\Framework\Controller\Result\JsonFactory;
 
 class Save extends \Magento\Framework\App\Action\Action
 {
-    protected $_resultFactory;
+    protected $resultJsonFactory; 
+     protected $_inlineTranslation;
 
-    public function __construct(Context $context
+    public function __construct(Context $context , 
+                               JsonFactory $resultJsonFactory,
+                               \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation
     )
     {
-        $this->_resultFactory = $context->getResultFactory();
-        parent::__construct($context);
+        $this->_inlineTranslation = $inlineTranslation;
+        $this->resultJsonFactory = $resultJsonFactory;
+        return parent::__construct($context);
     }
 
     public function execute()
     {
-        $postData = (array) $this->getRequest()->getPost();
+       $errors = false;
+       $message = "";
+
+       $postData = $this->getRequest()->getPostValue();
 
 
+       if(!$postData)
+       {
+            $errors = true;
+       }
 
-        $resultRedirect = $this->_resultFactory->create(ResultFactory::TYPE_REDIRECT);
+       $this->_inlineTranslation->suspend();
+       $postObject = new \Magento\Framework\DataObject();
+        $postObject->setData($postData);
 
-        $postData = (array) $this->getRequest()->getPost();
-        if (!empty($postData)) {
-            // Retrieve your form data
+         if (!\Zend_Validate::is(trim($postData['content']), 'NotEmpty')) {
+                $errors = true;
+            }
+
+          if (!\Zend_Validate::is(trim($postData['email']), 'EmailAddress')) {
+                $errors = true;
+            }
+
+
+        $result = $this->resultJsonFactory->create();
+
+        if($errors)
+        {
+            $message = "Wrong input field";
+            $result->setData(['result' => 'fail', 'message' => $message]);
+        }
+        else
+        {
+            $message = "submit success";
+
+              // Retrieve your form data
+            $email = $postData['email'];
             $author   = $postData['user_id'];
             $content    = $postData['content'];
             $post_id = $postData['post_id'];
 
 
             $comment = $this->_objectManager->create('OpenTechiz\Blog\Model\Comment');
+            $comment->setEmail($email);
             $comment->setUserId($author);
             $comment->setContent($content);
             $comment->setPostId($post_id);
+            $comment->setStatus(0);
+            $comment->setPending(0);
+            $comment->setDeny(0);
             $comment->save();
 
+            $result->setData(['result' => 'success', 'message' => $message]);
         }
 
-        $resultRedirect->setUrl($this->_redirect->getRefererUrl());
-        return $resultRedirect;
+
+        return $result;
     }
 
 }
